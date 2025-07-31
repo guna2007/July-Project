fea9accf924e15de5c92ae03ef39de53ab6d3ee8
guna2007 <lankalapalligunapersonal@gmail.com>
2025-07-31 20:39:47 +0530

    Dev:1 -> In Task Class with  init and basic structure

---
+++ tasks.py
@@ -0,0 +1,31 @@
+import datetime
+class Task:
+
+    id_count = 1
+    def __init__(self, task, category, *args):
+        self.task = task
+        self.category = str(category)
+        self.date_added = datetime.datetime.now().strftime("%d/%m/%Y::%H:%M")
+        self.date_due = None
+        self.completed = False
+
+        str_count = 0
+        manual_id = None
+
+        for arg in args:
+            if isinstance(arg, int):
+                manual_id = arg
+            elif isinstance(arg , str):
+                if str_count == 0:
+                    self.date_due = arg
+                    str_count+=1
+
+                    
+
+        if manual_id is not None:
+            self.id = manual_id
+        else:
+            self.id = Task.id_count
+            Task.id_count+=1
+
+   
